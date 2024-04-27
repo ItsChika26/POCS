bec45c2bd70b0157226c8075dea97472e88735c6
riylett <silasolak23@outlook.com>
2024-04-27 16:25:26 +0200

    sign in form

--- PPProject/Form1.Designer.cs
+++ PPProject/Form1.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             label_username = new Label();
             label_password = new Label();
             panel1 = new Panel();
-            panel2 = new Panel();
             textBox_user = new TextBox();
             label_title = new Label();
             button_register = new Button();
@@ -40,7 +40,6 @@
             textBox_pass = new TextBox();
             errorProvider_user = new ErrorProvider(components);
             panel1.SuspendLayout();
-            panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)errorProvider_user).BeginInit();
             SuspendLayout();
             // 
@@ -49,7 +48,7 @@
             label_username.AutoSize = true;
             label_username.Font = new Font("Arial", 12F, FontStyle.Bold);
             label_username.ForeColor = SystemColors.ControlLightLight;
-            label_username.Location = new Point(7, 14);
+            label_username.Location = new Point(27, 134);
             label_username.Name = "label_username";
             label_username.Size = new Size(112, 24);
             label_username.TabIndex = 0;
@@ -61,7 +60,7 @@
             label_password.AutoSize = true;
             label_password.Font = new Font("Arial", 12F, FontStyle.Bold);
             label_password.ForeColor = SystemColors.ControlLightLight;
-            label_password.Location = new Point(29, 168);
+            label_password.Location = new Point(29, 192);
             label_password.Name = "label_password";
             label_password.Size = new Size(110, 24);
             label_password.TabIndex = 1;
@@ -71,7 +70,8 @@
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(61, 44, 98);
-            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(textBox_user);
+            panel1.Controls.Add(label_username);
             panel1.Controls.Add(label_title);
             panel1.Controls.Add(button_register);
             panel1.Controls.Add(button_login);
@@ -83,23 +83,14 @@
             panel1.Size = new Size(382, 353);
             panel1.TabIndex = 2;
             // 
-            // panel2
-            // 
-            panel2.BackColor = Color.MidnightBlue;
-            panel2.Controls.Add(textBox_user);
-            panel2.Controls.Add(label_username);
-            panel2.Location = new Point(29, 93);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(324, 52);
-            panel2.TabIndex = 7;
-            // 
             // textBox_user
             // 
             textBox_user.BackColor = SystemColors.Window;
+            textBox_user.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
             textBox_user.ForeColor = SystemColors.WindowText;
-            textBox_user.Location = new Point(130, 14);
+            textBox_user.Location = new Point(145, 127);
             textBox_user.Name = "textBox_user";
-            textBox_user.Size = new Size(174, 27);
+            textBox_user.Size = new Size(205, 34);
             textBox_user.TabIndex = 2;
             // 
             // label_title
@@ -107,7 +98,7 @@
             label_title.AutoSize = true;
             label_title.Font = new Font("Arial", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 162);
             label_title.ForeColor = SystemColors.ControlLightLight;
-            label_title.Location = new Point(12, 18);
+            label_title.Location = new Point(17, 32);
             label_title.Name = "label_title";
             label_title.Size = new Size(353, 51);
             label_title.TabIndex = 6;
@@ -115,11 +106,16 @@
             // 
             // button_register
             // 
-            button_register.BackColor = SystemColors.ControlLightLight;
-            button_register.Font = new Font("Segoe UI", 12F);
-            button_register.Location = new Point(225, 249);
+            button_register.BackColor = Color.FromArgb(4, 189, 202);
+            button_register.Cursor = Cursors.Hand;
+            button_register.FlatAppearance.BorderSize = 0;
+            button_register.FlatStyle = FlatStyle.Flat;
+            button_register.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button_register.ForeColor = SystemColors.ControlLightLight;
+            button_register.Location = new Point(201, 250);
             button_register.Name = "button_register";
-            button_register.Size = new Size(117, 54);
+            button_register.Padding = new Padding(0, 0, 0, 3);
+            button_register.Size = new Size(149, 54);
             button_register.TabIndex = 5;
             button_register.Text = "Register";
             button_register.UseVisualStyleBackColor = false;
@@ -128,10 +124,15 @@
             // button_login
             // 
             button_login.BackColor = Color.FromArgb(4, 189, 202);
-            button_login.Font = new Font("Segoe UI", 12F);
-            button_login.Location = new Point(36, 249);
+            button_login.Cursor = Cursors.Hand;
+            button_login.FlatAppearance.BorderSize = 0;
+            button_login.FlatStyle = FlatStyle.Flat;
+            button_login.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button_login.ForeColor = SystemColors.ControlLightLight;
+            button_login.Location = new Point(27, 250);
             button_login.Name = "button_login";
-            button_login.Size = new Size(119, 54);
+            button_login.Padding = new Padding(0, 0, 0, 3);
+            button_login.Size = new Size(149, 54);
             button_login.TabIndex = 4;
             button_login.Text = "Login";
             button_login.UseVisualStyleBackColor = false;
@@ -139,10 +140,10 @@
             // 
             // textBox_pass
             // 
-            textBox_pass.Location = new Point(159, 168);
+            textBox_pass.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            textBox_pass.Location = new Point(145, 185);
             textBox_pass.Name = "textBox_pass";
-            textBox_pass.PasswordChar = '*';
-            textBox_pass.Size = new Size(174, 27);
+            textBox_pass.Size = new Size(205, 34);
             textBox_pass.TabIndex = 3;
             textBox_pass.UseSystemPasswordChar = true;
             // 
@@ -150,6 +151,7 @@
             // 
             errorProvider_user.BlinkStyle = ErrorBlinkStyle.NeverBlink;
             errorProvider_user.ContainerControl = this;
+            errorProvider_user.Icon = (Icon)resources.GetObject("errorProvider_user.Icon");
             // 
             // Form1
             // 
@@ -158,13 +160,13 @@
             BackColor = Color.Navy;
             ClientSize = new Size(382, 353);
             Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Sign In";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            panel2.ResumeLayout(false);
-            panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)errorProvider_user).EndInit();
             ResumeLayout(false);
         }
@@ -180,6 +182,5 @@
         private TextBox textBox_user;
         private Label label_title;
         private ErrorProvider errorProvider_user;
-        private Panel panel2;
     }
 }
